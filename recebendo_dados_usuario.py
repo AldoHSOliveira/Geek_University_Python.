67a1c141e55e7945a8992ba039b2349002f9546f
Aldo <aldohenrique72@gmail.com>
2023-02-10 10:26:32 -0300

    Recebendo dados

---
+++ recebendo_dados_usuario.py
@@ -0,0 +1,39 @@
+"""
+Recebendo dados do usuario
+"""
+# Entrada de dados
+print("Qual é seu nome ? ")
+nome = input() #input = entrada
+
+# Processamento
+
+
+# Saida de dados
+
+#Exemplo de print Antigo
+
+#print('Seja bem vindo[a] %s' % nome)
+
+#Exemplo de print Moderno
+#print('Seja bem vinda(a) {0}'.format(nome))
+
+#Exemplo de print atual
+print(f'Seja bem vindo {nome}')
+
+# Entrada de dados
+
+print('Qual sua idade? ')
+idade = input()
+
+# Processamento
+
+#Saida de dados
+
+#Exemplo de print Antigo
+#print('Você %s tem %s anos' % (nome, idade))
+
+#Exemplo de print Moderno
+#print('Você {0} tem {1} anos '.format(nome,idade))
+
+#Exemplo de print atual
+print(f'voce {nome} tem {idade} anos')
