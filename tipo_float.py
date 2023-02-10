efc3d43ff743a99fdf0c663c24732646f961061c
Aldo <aldohenrique72@gmail.com>
2023-02-10 13:48:01 -0300

    Tipo int, operadores e Float

---
+++ tipo_float.py
@@ -0,0 +1,14 @@
+"""Tipo Float
+
+Casas decimais
+obs: O separador de casas decimais na programação é o ponto e não virgula
+
+# Errado
+valor = 1,44
+
+# Certo
+
+valor = 1.44
+"""
+
+
---
+++ tipo_numerico.py
@@ -0,0 +1,27 @@
+"""
+Tipo numérico
+
+Int = inteiros
+"""
+# O Python permite o underline para facilitar a visualização de um número
+num = 1_000_0000
+print(num)
+
+
+#operadores
+
+soma = 2+1
+subtrair = 2-1
+multiplicar = 2*1
+dividir= 2//1
+potencia = 2**1
+resto = 2%1
+
+#imprimir na tela
+
+print(f'soma',soma)
+print(f'subtrair',subtrair)
+print(f'multiplicar',multiplicar)
+print(f'dividir',dividir)
+print(f'potencia',potencia)
+print(f'resto',resto)
