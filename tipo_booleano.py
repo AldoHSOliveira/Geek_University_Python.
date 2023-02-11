3481f946e524f29dcd573d2a73a9a6d19fac1cbc
Aldo <aldohenrique72@gmail.com>
2023-02-11 15:51:38 -0300

    tipo_booleano

---
+++ tipo_booleano.py
@@ -0,0 +1,38 @@
+"""
+Tipo Booleano
+Algebra Booleana, criada por George Bool
+2 costantes, Verdadeiro e Falso
+True -> Verdadeiro
+False -> falso
+obs: Sempre com a inicial maiúsculo
+"""
+ativo = True
+
+print(ativo)
+
+"""
+Operações básicas
+"""
+# negação (not):
+"""
+Fazendo a negação , se o valor  booleano for verdadeiro o resultado será false, se for falso o resultado será verdadeiro, ou seja, sempre o contrário
+"""
+print(not ativo) # Fazendo a negação
+
+
+# Ou (or):
+""" Operação binária , ou seja, depende de dois valores , ou um ou outro tem que ser verdadeiro
+True or True -> True
+True or False -> True
+False or True -> True
+False or False -> False
+"""
+
+# (and):
+"""
+Também e em uma operação binaria , ou seja, depende de dois valores, ambos os valores devem ser verdadeiro
+True and True -> True
+True and False -> False
+False and True -> False
+False and False -> False
+"""
