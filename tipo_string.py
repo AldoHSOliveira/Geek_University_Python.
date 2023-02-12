9aa62e761d664b68e2256f6103562d949900043c
Aldo <aldohenrique72@gmail.com>
2023-02-12 12:24:15 -0300

    Tipo_string

---
+++ tipo_string.py
@@ -0,0 +1,21 @@
+"""
+Tipo string
+
+Em Python, um dado é considerado do tipo string sempre que estiver
+
+ Entre aspas simples '121' 'a' 'True' '43.2'
+ Entre aspas duplas "123" "a" "43.3"
+ Entre aspas simpls triplas '''123''' '''a''' ....
+ entre aspas duplas triplas...
+
+"""
+#todas as formas de aspas abaixo funciona da mesma forma e são do tipo string
+
+nome = 'Python'
+print(nome)
+name = "Python"
+print(name)
+valor = '''Python'''
+print(valor)
+nick = """Python"""
+print(nick)
